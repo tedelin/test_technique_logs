0047aee7544d7538a9dee31588b959492472363e
Tanguy EDELIN <tedelin@users.noreply.github.com>
2025-07-15 13:57:13 +0200

    init front/back with docker

---
+++ frontend/vite.config.ts
@@ -0,0 +1,14 @@
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server: {
+    port: 5173,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:5173",
+  },
+});
