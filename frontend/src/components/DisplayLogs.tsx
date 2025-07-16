94806625c8b5f04713b8729379798f8457d3460b
Tanguy EDELIN <tedelin@users.noreply.github.com>
2025-07-16 15:03:54 +0200

    Add component to display logs

---
+++ frontend/src/components/DisplayLogs.tsx
@@ -0,0 +1,21 @@
+import type { Log } from "@/lib/types";
+import { useEffect } from "react";
+
+export default function DisplayLogs({ logs }: { logs: Log[] }) {
+  useEffect(() => {}, [logs]);
+
+  return (
+    <>
+      {logs.map((log: Log) => (
+        <div className="grid grid-cols-4 text-center">
+          <div className="text-left">
+            {new Date(log.timestamp).toLocaleString()}
+          </div>
+          <div>{log.message}</div>
+          <div>{log.level}</div>
+          <div>{log.service}</div>
+        </div>
+      ))}
+    </>
+  );
+}
