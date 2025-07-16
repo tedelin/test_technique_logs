6aa5430e700de0724b6de18c5b16af0cd13af120
Tanguy EDELIN <tedelin@users.noreply.github.com>
2025-07-16 11:32:55 +0200

    Add cors origin policy, rm console log

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,16 +4,27 @@ from app.models.log import Log
 from fastapi import FastAPI, Body
 from datetime import datetime
 from typing import Annotated
+from fastapi.middleware.cors import CORSMiddleware
 import os
 
 app = FastAPI()
+origins = [
+    "http://localhost:5173",
+    "https://frontend:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
+)
 host = os.getenv("OPENSEARCH_HOST", "http://opensearch-node:9200")
 client = OpenSearch(hosts=[host])
 
 @app.post("/logs/")
 async def create_logs(log: Annotated[Log, Body(embed=True)]):
-    dt = datetime.fromisoformat(log.timestamp.replace("Z", "+00:00"))
-    index = f"logs-{dt.year}.{dt.month}.{dt.day}"
+    index = f"logs-{log.timestamp.year}.{log.timestamp.month}.{log.timestamp.day}"
     response = client.index(index=index, body=log.dict())
-    print(log, response)
     return {"log": log, "id": response["_id"]}
