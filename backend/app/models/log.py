9919717e35a7fbb04d8c7c23c529b394c1c4dad4
Tanguy EDELIN <tedelin@users.noreply.github.com>
2025-07-16 11:32:18 +0200

    Improve validation with builtin type

--- backend/app/models/log.py
+++ backend/app/models/log.py
@@ -2,18 +2,10 @@ from pydantic import BaseModel, Field, validator
 from datetime import datetime
 
 class Log(BaseModel):
-    timestamp: str
+    timestamp: datetime
     message: str
     level: str
     service: str
-
-    @validator('timestamp')
-    def validate_timestamp(cls, value):
-        try:
-            datetime.fromisoformat(value.replace("Z", "+00:00"))
-            return value
-        except ValueError:
-            raise ValueError("Timestamp must be a valid ISO 8601 datetime string")
 
     @validator('level')
     def validate_level(cls, value):
