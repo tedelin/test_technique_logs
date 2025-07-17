4e87699c3c5b66ef842784d95b23e7e1146c48b6
Tanguy EDELIN <tedelin@users.noreply.github.com>
2025-07-17 14:53:20 +0200

    Fix layout issue

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -1,12 +1,14 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-dvh max-h-dvh min-h-screen flex-col bg-zinc-900">
+    <div className="m-0 flex h-screen flex-col overflow-hidden bg-zinc-900">
       <header className="flex h-16 items-center border-b pl-6">
         <h1 className="text-xl font-bold">📮 Logs Dashboard</h1>
       </header>
 
       <div className="flex flex-1 overflow-hidden">
-        <main className="flex-1 p-6">{children}</main>
+        <main className="flex flex-1 flex-col overflow-hidden p-6">
+          {children}
+        </main>
       </div>
     </div>
   );
